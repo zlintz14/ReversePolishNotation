b13ecec4753656226e1422604b2f416bc03d248d
zlintz14 <zakl14@live.unc.edu>
2018-05-01 11:44:30 -0400

    Added support for exponentials

--- src/RPNInterpreter.ts
+++ src/RPNInterpreter.ts
@@ -12,12 +12,12 @@ let main = (): void => {
 
     var prompt = require("prompt-sync")();
     console.log("\nRules for data entry:\n 1) ALWAYS enter a space between digits and operators. \n 2) IF you have any double digit numbers, i.e. 12, 100, 5000, etc., you MUST use spaces between terms. \n EXAMPLE: if you want to evaluate: (23 + 48) * 12, enter: 12 48 23 +*\n 3) IF you have four or more terms you MUST use spaces even if they are all single digits and you MUST enter the first two terms followed by an operator, then thereafter enter each additional term by itself followed by an operator.\n EXAMPLE: to evaluate: (12 * 3 + 4) / 10, enter: 3 12 * 4 + 10 / \n 4) IF you have three terms or less AND all are single digits you may enter spaces or no spaces between terms.\n EXAMPLE: 123 +/ \n EXAMPLE: 1 2 3 +/ \n Both are valid inputs and equivalent to each other.\n 5) VARYING spaces between terms do not matter, so long as there are no spaces between consecutive operators. \n EXAMPLE: (12 * 3 + 4) / 10, can be entered as: 3      12 *  4 +   10 / and  (3 + 2) / 1 can be entered as: 1 2    3 +/ \n")
-    var RPN: string = prompt("Enter you function is Reverse Polish Notation (use *,/,+,- for operators): ")
+    var RPN: string = prompt("Enter you function is Reverse Polish Notation (use *,/,+,-,^ for operators): ")
     
     //loop through entire input and perform appropriate operations
     for(let i = 0; i < RPN.length; i++) {
         //determine if current token is an operator
-        if(RPN[i] === "+" || RPN[i] === "-" || RPN[i] === "/" || RPN[i] === "*" || RPN[i] === " ") {
+        if(RPN[i] === "+" || RPN[i] === "-" || RPN[i] === "/" || RPN[i] === "*" || RPN[i] === "^"|| RPN[i] === " ") {
             switch(RPN[i]) {
                 case("+"):
                     //if stack's length is one do something slightly different for formatting reasons and perform appropriate operations on current total
@@ -89,6 +89,23 @@ let main = (): void => {
                         interpreted += "(" + num1 + " / " + num2 + ")"
                     }
                     break
+                case("^"):
+                    if(stack.length === 1) {
+                        num1 = stack.pop()
+                        if(num1 != undefined) {
+                            total = total ** num1
+                        } 
+                        interpreted += " ^ " + num1 + ")"
+                        parenthesisCount++
+                    } else {
+                        num1 = stack.pop()
+                        num2 = stack.pop()
+                        if(num1 != undefined && num2 != undefined) {
+                            total = num1 ** num2
+                        } 
+                        interpreted += "(" + num1 + " ^ " + num2 + ")"
+                    }
+                    break    
                 default:
                     break
             }
@@ -134,8 +151,8 @@ let main = (): void => {
 //determine if the input entered was a collection of single digit numbers with no spacing, used for 3 or less terms
 let determineNoSpaceSingleDigitInput = (input: string): boolean => {
     for(let i = 0; i < input.length; i++) {
-        if((input[i] === "+" || input[i] === "-" || input[i] === "*" || input[i] === "/") && 
-            (input[i + 1] === "+" || input[i + 1] === "-" || input[i + 1] === "*" || input[i + 1] === "/")) {
+        if((input[i] === "+" || input[i] === "-" || input[i] === "*" || input[i] === "/" || input[i] === "^") && 
+            (input[i + 1] === "+" || input[i + 1] === "-" || input[i + 1] === "*" || input[i + 1] === "/" || input[i + 1] === "^")) {
             return true
         }
     }
